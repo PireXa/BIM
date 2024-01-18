28f3728f67b46667443afe10840e740be918f50b
Francisco Pires <98482270+PireXa@users.noreply.github.com>
2024-01-18 16:02:40 +0000

    Added main GLM header

---
+++ include/glMath.hpp
@@ -0,0 +1,13 @@
+//
+// Created by pirexa on 18/01/24.
+//
+
+#ifndef BIM_GLMATH_HPP
+#define BIM_GLMATH_HPP
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include <glm/gtc/type_ptr.hpp>
+
+#endif //BIM_GLMATH_HPP
