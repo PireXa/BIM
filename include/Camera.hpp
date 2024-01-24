a0ce43d6958943a00a1a07fe7d6295c80f6a6664
Francisco Pires <98482270+PireXa@users.noreply.github.com>
2024-01-24 22:03:47 +0000

    Fixed Camera Rotation

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -8,19 +8,28 @@
 #include <cmath>
 #include "glMath.hpp"
 #include "Quaternion.hpp"
-#define STEP 4.0f
-#define SENSITIVITY 0.002f
+#define SENSITIVITY 0.001f
 
 class Camera {
     private:
         glm::quat   orientation;
         glm::vec3   position;
+		float       moveSpeed;
     public:
         Camera() : orientation(), position() {
             orientation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
             position = glm::vec3(-150.0f, 150.0f, 250.0f);
-//			position = glm::vec3(-2.0f, 1.5f, 5.0f);
+			moveSpeed = 0.5f;
         }
+
+		float getMoveSpeed() {
+			return moveSpeed;
+		}
+
+		void setMoveSpeed(float speed) {
+			moveSpeed = speed;
+		}
+
         void    rotate(float angle, float x, float y, float z) {
             glm::vec3 axis = glm::normalize(glm::vec3(x, y, z));
             glm::quat rotation = glm::angleAxis(angle, axis);
@@ -30,20 +39,26 @@ class Camera {
 		void    rotate_pitch(float pitch) {
 			glm::vec3 axis = glm::normalize(glm::vec3(-1.0f, 0.0f, 0.0f));
 			glm::quat rotation = glm::angleAxis(pitch, axis);
-			orientation = rotation * orientation;
+			orientation = glm::normalize(rotation * orientation);
 		}
 
-		void	rotate_yaw(float yaw) {
-			glm::vec3 axis = glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f));
-			glm::quat rotation = glm::angleAxis(yaw, axis);
-			orientation = rotation * orientation;
+		void rotate_yaw(float yaw) {
+			// Specify the rotation axis in world coordinates
+			glm::vec3 worldYawAxis = glm::vec3(0.0f, 1.0f, 0.0f);
+
+			// Apply the rotation to the orientation quaternion
+			orientation = glm::rotate(orientation, yaw, worldYawAxis);
+
+			// Ensure the quaternion is normalized to prevent unwanted drift
+			orientation = glm::normalize(orientation);
 		}
 
-        void    translate(float x, float y, float z) {
-            position.x += x;
-            position.y += y;
-            position.z += z;
-        }
+
+		void    translate(float x, float y, float z) {
+	            position.x += x;
+	            position.y += y;
+	            position.z += z;
+	        }
 
         glm::mat4   getViewMatrix()
         {
--- include/Input.hpp
+++ include/Input.hpp
@@ -12,18 +12,17 @@
 class Input {
     public:
         static bool keys[1024];
-        static bool keysProcessed[1024];
-        static bool firstMouse;
         static double lastX;
         static double lastY;
         static float yaw;
         static float pitch;
         static float fov;
+		static float moveSpeed;
 
         static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
         static void mouseCallback(GLFWwindow* window, double xpos, double ypos);
-//        static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-        static void doMovement(GLFWwindow *window, Camera &camera);
+        static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+        static void doMovement(Camera &camera);
 };
 
 #endif //BIM_INPUT_HPP
