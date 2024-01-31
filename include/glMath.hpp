b087fc186bdb1410f78503d304d915294054c8f6
Francisco Pires <98482270+PireXa@users.noreply.github.com>
2024-01-31 12:51:11 +0000

    Added a class for reading the .obj file

--- include/glMath.hpp
+++ include/glMath.hpp
@@ -5,11 +5,17 @@
 #ifndef BIM_GLMATH_HPP
 #define BIM_GLMATH_HPP
 
-#include "GLM/glm.hpp"
-#include "GLM/gtc/matrix_transform.hpp"
-#include "GLM/gtc/quaternion.hpp"
-#include "GLM/gtc/type_ptr.hpp"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include <glm/gtc/type_ptr.hpp>
 
 #include "GLFW/glfw3.h"
+
+struct Vertex {
+	float x, y, z;
+//    float u, v; // Texture coordinates
+//    float nx, ny, nz; // Normals
+};
 
 #endif //BIM_GLMATH_HPP
---
+++ include/readOBJ.hpp
@@ -0,0 +1,37 @@
+//
+// Created by kikop on 31/01/2024.
+//
+
+#ifndef BIM_READOBJ_HPP
+#define BIM_READOBJ_HPP
+
+#include <iostream>
+#include <vector>
+#include "glMath.hpp"
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+class readOBJ {
+	private:
+		const char *filename;
+		std::vector<Vertex> vertices;
+		float *verticesArray;
+		int verticesArraySize;
+		int faceCount;
+		int vertexCount;
+	public:
+		readOBJ(const char *filename) : filename(filename) {
+			readVertices();
+			readFaces();
+		}
+		~readOBJ();
+		float *getVerticesArray();
+		int getVerticesArraySize();
+		int getVertexCount();
+		int getFaceCount();
+		void readVertices();
+		void    readFaces();
+};
+
+#endif //BIM_READOBJ_HPP
